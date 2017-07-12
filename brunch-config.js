2244b25b4a8aca3825a35869bf75a28cb21f4d32
dmix <dan@dmix.ca>
2017-07-11 23:41:50 -0400

    Add eslint and postcss

--- brunch-config.js
+++ brunch-config.js
@@ -1,66 +1,78 @@
 exports.config = {
-  // See http://brunch.io/#documentation for docs.
-  files: {
-    javascripts: {
-      joinTo: "js/app.js"
+    // See http://brunch.io/#documentation for docs.
+    files: {
+        javascripts: {
+            joinTo: 'js/app.js'
+        },
+        stylesheets: {
+            joinTo: 'css/app.css',
+            order: {
+                after: ['web/static/css/app.css'] // concat app.css last
+            }
+        },
+        templates: {
+            joinTo: 'js/app.js'
+        }
     },
-    stylesheets: {
-      joinTo: "css/app.css",
-      order: {
-        after: ["web/static/css/app.css"] // concat app.css last
-      }
+
+    conventions: {
+        // This option sets where we should place non-css and non-js assets in.
+        // By default, we set this to '/web/static/assets'. Files in this directory
+        // will be copied to `paths.public`, which is 'priv/static' by default.
+        assets: /^(web\/static\/assets)/
     },
-    templates: {
-      joinTo: "js/app.js"
-    }
-  },
 
-  conventions: {
-    // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/web/static/assets". Files in this directory
-    // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(web\/static\/assets)/
-  },
+    // Phoenix paths configuration
+    paths: {
+        // Dependencies and current project directories to watch
+        watched: [
+            'web/static',
+            'test/static'
+        ],
 
-  // Phoenix paths configuration
-  paths: {
-    // Dependencies and current project directories to watch
-    watched: [
-      "web/static",
-      "test/static"
-    ],
-
-    // Where to compile files to
-    public: "priv/static"
-  },
-
-  // Configure your plugins
-  plugins: {
-    babel: {
-      // Do not use ES6 compiler in vendor code
-      ignore: [/web\/static\/vendor/]
+        // Where to compile files to
+        public: 'priv/static'
     },
-    sass: {
-      options: {
-        includePaths: [
-          'node_modules/foundation-sites/scss',
-          'node_modules/motion-ui/src',
-        ]
-      }
-    }
-  },
 
-  modules: {
-    autoRequire: {
-      "js/app.js": ["web/static/js/app"]
-    }
-  },
+    // Configure your plugins
+    plugins: {
+        babel: {
+            // Do not use ES6 compiler in vendor code
+            ignore: [/web\/static\/vendor/]
+        },
+        sass: {
+            options: {
+                includePaths: [
+                    'node_modules/foundation-sites/scss',
+                    'node_modules/motion-ui/src',
+                ]
+            }
+        },
+        postcss: {
+            processors: [
+                require('autoprefixer')(['last 8 versions']),
+                require('postcss-import')(),
+                require('postcss-mixins')(),
+                require('postcss-custom-media')(),
+                require('postcss-custom-properties')(),
+                require('postcss-calc')(),
+                require('postcss-color-function')(),
+                require('postcss-discard-comments')(),
+            ]
+        }
+    },
 
-  npm: {
-    enabled: true,
-    globals: {
-      $: 'jquery',
-      jQuery: 'jquery',
+    modules: {
+        autoRequire: {
+            'js/app.js': ['web/static/js/app']
+        }
+    },
+
+    npm: {
+        enabled: true,
+        globals: {
+            $: 'jquery',
+            jQuery: 'jquery',
+        }
     }
-  }
-};
+}
